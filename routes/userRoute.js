6155c396a7eab3d5776d1fffcb1fb054e3ce0b13
pradeep <pradeepreddyp1997@gmail.com>
2023-03-14 16:54:36 +0530

    API Changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,10 @@
 const express=require('express')
 const router=express.Router()
-const {registerController,logoutController, userProfileController}=require('../controllers/userController')
+const {registerController,logoutController, getBlogsByUser}=require('../controllers/userController')
 const {loginController} =require('../controllers/userController')
 const isAuthorized = require("../middleware/auth");
 router.post("/register",registerController)
 router.post("/login",loginController)
-router.get('/profile',isAuthorized,userProfileController)
+router.get('/blogs',isAuthorized,getBlogsByUser)
 router.get('/logout',logoutController)
 module.exports=router
