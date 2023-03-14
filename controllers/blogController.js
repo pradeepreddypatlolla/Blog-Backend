1503e72a73327a5fe3a0c6874663d882416dc3f3
pradeep <pradeepreddyp1997@gmail.com>
2023-03-14 16:37:00 +0530

    API Changes

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -7,7 +7,7 @@ const User = require('../models/userModel');
     console.log(req.body.data);
     try {
         const {_id}=req.user
-        console.log(_id)
+       
        let blog= new Blog({userId:_id, title:req.body.title ,content:req.body.content , comments:[],imgUrls:req.body.imgUrls})
        let newBlog=await blog.save()
        res.status(200).json({
@@ -31,17 +31,14 @@ const blogUpdateController=async(req,res,next)=>{
     console.log(req.body.data);
     try {
         const {emailId}=req.user
-        console.log(emailId);
-        console.log(req.body);
+       
         let blogId=req.body.blogId
        let blog= await Blog.findByIdAndUpdate({_id:blogId},
         {
             title:req.body.title,content:req.body.content,imgUrls:req.body.imgUrls
        },
        )
-       console.log(blog);
-      // blog.update({title:req.body.title,content:req.body.content,imgUrls:req.body.imgUrls})
-       
+
        res.status(200).json({
         success:true,
         blog
@@ -64,7 +61,7 @@ const blogDeleteController=async(req,res,next)=>{
         let blogId=req.body.blogId
         let blog=await Blog.findById(blogId)
         let imgUrls=blog.imgUrls
-        //console.log(imgUrls);
+        
         for(let i=0;i<imgUrls.length && imgUrls[i]!==null;i++){
             let splitURL=imgUrls[i].split('/')
             //console.log(splitURL[splitURL.length-1].split('.')[0]);
@@ -94,9 +91,9 @@ const blogDeleteController=async(req,res,next)=>{
 
 const blogCommentsSubmitController=async (req,res)=>{
     try {
-        console.log(req.body);
+       
         const blogId=req.body.blogId
-        console.log(blogId);
+       
         let blog=await Blog.findById(blogId)
         console.log(blog);
         blog.comments.push({comment:req.body.comment,user:req.user.emailId})
@@ -115,8 +112,7 @@ const blogCommentsSubmitController=async (req,res)=>{
 const blogUploadPhoto=(req,res,next)=>{
 
 
-    console.log(req.body.data);
-    console.log('backend ')
+   
     cloudinary.uploader.upload(req.body.data,{
         upload_preset:'aqaepvlw'
     }).then((result) => {
@@ -125,7 +121,7 @@ const blogUploadPhoto=(req,res,next)=>{
           url:result.url,
         });
       }).catch((error) => {
-        console.log(error);
+        
         res.status(500).send({
           message: "failure",
           error,
@@ -133,43 +129,13 @@ const blogUploadPhoto=(req,res,next)=>{
       });
 
 
-//         console.log(req.file)
-       
-//         let fd={}
-//         fd.file=req.file
-//         fd.upload_preset='aqaepvlw'
-    
-
-//     let options = {
-//         method: 'POST',
-//         uri: process.env.CLOUDINARY_URL,
-//         formData: fd, // formData here is an object
-//         headers: {
-//             'Access-Control-Allow-Origin': '*',
-//             'enctype': 'multipart/form-data',
-//             'Content-Type': 'application/json',
-//             'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
-//         },
-//         json: true
-//     };
-  
-// request(options, function(error, response, body){
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log(response.statusCode, body);
-//     }
-//  });
-    
-    //  res.send({filename:"../blogimages/"+file.filename});
-
 
 }
 const getBlogs=async (req,res)=>{
     try{
-        const {_id}=req.user
+      
         
-        let blogs=await Blog.find({userId:_id})
+        let blogs=await Blog.find()
         console.log(blogs);
         res.status(200).json({
             success:true,
@@ -185,6 +151,8 @@ const getBlogs=async (req,res)=>{
         
     }
 }
+
+
 
 const getBlogById=async (req,res)=>{
 
@@ -206,9 +174,7 @@ const getBlogById=async (req,res)=>{
     }
 
 
-    // res.send(`
-    // Hello World&nbsp;<br>How are you?<br><br><img src="../blogimages/1665244999247testimg.png"> <img src="../blogimages/1665244999268testimg.png">
-    // `)
+  
 }
 
  
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,5 @@
 const User=require('../models/userModel')
 const Blog=require('../models/blogModel')
-// const file=require('../views/login.html')
-const path=require('path')
 const registerController=async function(req,res,next){
 
     try {
@@ -35,8 +33,8 @@ const loginController=async function(req,res,next){
     try {
         
         const {emailId,password}=req.body
-        console.log(emailId,password);
-        let user=await User.findOne({emailId:emailId})
+        
+        let user=await User.findOne({emailId:emailId});
         if(!user){
            return res.status(400).json({
                 success:false,
@@ -51,22 +49,15 @@ const loginController=async function(req,res,next){
             })
         }
         let token=await user.generateToken();
-        console.log(token);
-    //    let cookieOption={expies:new Date(Date.now())+5000,
+        delete user.password; // to remove password so that its not sent in response
        
-    //     httpOnly: true,
-    //     sameSite: 'None',
-    //     secure: true,}
-    //    console.log(token);
-    //     res.status(200).cookie("token",token,cookieOption)
-       // console.log(res.cookies);
         res.json({
             success:true,
             user,
             token
         })
 
-      //  res.status(200).cookie("token",token,cookieOption).redirect('/blog/editor')
+     
 
     } catch (error) {
         res.status(500).json({
@@ -77,12 +68,12 @@ const loginController=async function(req,res,next){
     }
 
 }
-const userProfileController=async (req,res,next)=>{
+const getBlogsByUser=async (req,res,next)=>{
 
     try {
-        const {emailId}=req.user
-        let blogs= await Blog.find({email:emailId})
-        console.log(blogs);
+        const {_id}=req.user
+        let blogs= await Blog.find({userId:_id})
+       
     
         res.status(200).json(
             {success:true,
@@ -105,4 +96,4 @@ const logoutController=async (req,res,next)=>{
   res.clearCookie("token").status(200).json({success:true,message:"Logged out!"})
 
 }
-module.exports={registerController,loginController,userProfileController,logoutController}
+module.exports={registerController,loginController,getBlogsByUser,logoutController}
